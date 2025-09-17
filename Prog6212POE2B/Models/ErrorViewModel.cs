e705e01384641ffb60e66c69d6b68c8b00e9e4b9
Ethan <128787335+ChippsEthan@users.noreply.github.com>
2025-09-17 22:34:23 +0200

    SUBMIT 1
    CREATED HOME

---
+++ Prog6212POE2B/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prog6212POE2B.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
